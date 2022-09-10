737cf3f0acd3f6f8754ca73e72023bc3c92aff19
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-09-10 17:03:01 +0300

    Added homework task after seminar 2

---
+++ Seminars/S2T1_subsequence/S2T1.java
@@ -0,0 +1,28 @@
+package Seminars.S2T1_subsequence;
+
+/**
+ * Найти общую подпоследовательность для двух заданных последовательностей.
+ */
+public class S2T1 {
+    public static void main(String[] args) {
+        String str =  "00000100012";
+        String str2 = "001231000121";
+        //0000100012
+        System.out.println(getSet(str, str2).length());
+    }
+    public static String getSet(String str, String str2){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0, lastInd = 0; i < str.length(); i++) {
+            for (int j = lastInd; j < str2.length(); j++) {
+                if (str.charAt(i) == str2.charAt(j)){
+                    stringBuilder.append(str2.charAt(j));
+                    lastInd = j + 1;
+                    break;
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+    
+}
+
